6393a4d2318c36bec21569b2dec686b47d048b61
Ujjal0011 <ujjal.0011@gmail.com>
2025-06-26 10:27:11 +0600

    blade directives

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -8,11 +8,34 @@
     <title>Contact</title>
 </head>
 
+@php
+    $title = 'Contact page';
+@endphp
+
 <body>
-    <h1>Contact Page</h1>
+    <h1>{{ $title }}</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, in corporis repudiandae sit iste cupiditate
         blanditiis necessitatibus quas odit. Sunt, iure eos laborum aut nihil reiciendis inventore consequatur nobis
         maxime?</p>
 </body>
 
 </html>
+
+{{-- @if ()
+
+@endif
+
+@if ()
+
+@else
+
+@endif
+
+@for ($i = 0; $i < $count; $i++)
+
+@endfor
+
+@foreach ($collection as $item)
+
+@endforeach
+ --}}
