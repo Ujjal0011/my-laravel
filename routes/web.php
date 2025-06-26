abf0930f2ac69ad220e212ef33388e2fad433519
Ujjal0011 <ujjal.0011@gmail.com>
2025-06-26 08:37:37 +0600

    fallback routes

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,10 @@ Route::delete('delete-route', function () {
     return;
 });
 
+Route::fallback(function () {
+    return 'page Not Found!';
+});
+
 // breeze
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
