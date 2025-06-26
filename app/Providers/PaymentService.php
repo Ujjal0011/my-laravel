336ca338a7656ce6d3f970021275fe02e581ee23
Ujjal0011 <ujjal.0011@gmail.com>
2025-06-26 07:25:33 +0600

    named route and route parameters

---
+++ app/Providers/PaymentService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class PaymentService
+{
+    public function process(): bool
+    {
+        echo 'Paid' . PHP_EOL;
+        return true;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/about', function () {
+    return 'This is about page';
+})->name('home.about');
 
+Route::get('/user/{id}/{slug}', function ($id, $slug) {
+    return 'Hello User: ' . $id . '; Slug: ' . $slug;
+})->name('home.user');
+
+
+// breeze
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
