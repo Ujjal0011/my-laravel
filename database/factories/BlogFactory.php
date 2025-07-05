af489583812883b0464cc3e52eaa1591f8aeee91
Ujjal0011 <ujjal.0011@gmail.com>
2025-07-05 13:39:41 +0600

    insert some fake data in the table

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -1,25 +1,22 @@
 <?php
 
+// database/factories/BlogFactory.php
 namespace Database\Factories;
 
+use App\Models\Blog;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
- */
 class BlogFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Blog::class;
+
     public function definition(): array
     {
         return [
-            'title' => fake()->sentence(10),
-            'description' => fake()->paragraph(),
-            'status' => fake()->boolean()
+            'title' => $this->faker->sentence(10),
+            'description' => $this->faker->paragraph(),
+            'author' => $this->faker->name(),
+            'status' => $this->faker->boolean(),
         ];
     }
 }
--- database/migrations/2025_06_28_023525_create_blogs_table.php
+++ database/migrations/2025_06_28_023525_create_blogs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->string('author');
+            $table->string('author')->default('Anonymous');
             $table->boolean('status');
             $table->timestamps();
         });
