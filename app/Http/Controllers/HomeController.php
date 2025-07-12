bd628c9c47c930a9ebb848ae50ed323c4a4ff45d
Ujjal0011 <ujjal.0011@gmail.com>
2025-07-12 12:22:23 +0600

    creating / inserting new data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index() {
+        DB::table('users')->insert([
+            'name'=>'Ujjl0011',
+            'email'=>'ujjal.0011@yahoo.com',
+            'password'=>'123456'
+        ]);
         return view('welcome');
     }
 
